4650458557962ae93b637b813d399835ba96b712
Ted Leung <twl@apache.org>
2000-01-22 01:17:18 +0000

    Get version number ready for 1.0.2 PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315198 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces 1.0.1";
+    public static String  fVersion = "Xerces 1.0.2";
 
     //
     // MAIN
