8e71564a8690482b3aae1c5d5d05587cf959ff8e
Andy Clark <andyc@apache.org>
2000-02-04 20:46:15 +0000

    Off by 1 error in the DeferredDocumentImpl caused an ArrayIndexOutOfBoundsException to occur if the number of identifiers was equal to the size of the identifier index array.
    This problem was reported by Karen Schuchardt
    <kl_schuchardt@pnl.gov> and resolved with the help
    (and persistence) of George Joseph <gtj@peakin.com>.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315239 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/DeferredDocumentImpl.java
+++ src/org/apache/xerces/dom/DeferredDocumentImpl.java
@@ -816,7 +816,7 @@ public class DeferredDocumentImpl
                 // check to see if this element needs to be
                 // registered for its ID attributes
                 if (fIdElement != null) {
-                    int idIndex = DeferredDocumentImpl.binarySearch(fIdElement, 0, fIdCount, nodeIndex);
+                    int idIndex = DeferredDocumentImpl.binarySearch(fIdElement, 0, fIdCount - 1, nodeIndex);
                     while (idIndex != -1) {
 
                         if (DEBUG_IDS) {
