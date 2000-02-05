0f36ff006d0a504ade673f267cbc809643f7ee22
Arnaud Le Hors <lehors@apache.org>
2000-02-05 02:26:39 +0000

    added messages for:  org.apache.html, org.apache.xml.serialize, and org.apache.xerces.dom
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315243 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/msg/ExceptionMessages.java
+++ src/org/apache/xerces/msg/ExceptionMessages.java
@@ -70,6 +70,51 @@ public class ExceptionMessages extends ListResourceBundle {
     public static final Object CONTENTS[][] = {
 // Internal message formatter messages
 
+	// org.apache.html
+
+	{ "HTM001", "State error: startDocument fired twice on one builder." },
+	{ "HTM002", "State error: document never started or missing document element." },
+	{ "HTM003", "State error: document ended before end of document element." },
+	{ "HTM004", "Argument 'tagName' is null." },
+	{ "HTM005", "State error: Document.getDocumentElement returns null." },
+	{ "HTM006", "State error: startElement called after end of document element." },
+	{ "HTM007", "State error: endElement called with no current node." },
+	{ "HTM008", "State error: mismatch in closing tag name {0}" },
+	{ "HTM009", "State error: character data found outside of root element." },
+	{ "HTM010", "State error: character data found outside of root element." },
+	{ "HTM011", "Argument 'topLevel' is null." },
+	{ "HTM012", "Argument 'index' is negative." },
+	{ "HTM013", "Argument 'name' is null." },
+	{ "HTM014", "Argument 'title' is null." },
+	{ "HTM015", "Tag '{0}' associated with an Element class that failed to construct." },
+	{ "HTM016", "Argument 'caption' is not an element of type <CAPTION>." },
+	{ "HTM017", "Argument 'tHead' is not an element of type <THEAD>." },
+	{ "HTM018", "Argument 'tFoot' is not an element of type <TFOOT>." },
+	{ "HTM019", "OpenXML Error: Could not find class {0} implementing HTML element {1}" },
+
+	// org.apache.xml.serialize
+
+	{ "SER001", "Argument 'output' is null." },
+	{ "SER002", "No writer supplied for serializer" },
+	{ "SER003", "The resource [{0}] could not be found." },
+	{ "SER004", "The resource [{0}] could not load: {1}" },
+	{ "SER005", "The method '{0}' is not supported by this factory" },
+
+	// org.apache.xerces.dom
+
+	{ "DOM001", "Modification not allowed" },
+	{ "DOM002", "Illegal character" },
+	{ "DOM003", "Namespace error" },
+	{ "DOM004", "Index out of bounds" },
+	{ "DOM005", "Wrong document" },
+	{ "DOM006", "Hierarchy request error" },
+	{ "DOM007", "Not supported" },
+	{ "DOM008", "Not found" },
+	{ "DOM009", "Attribute already in use" },
+	{ "DOM010", "Unspecified event type" },
+	{ "DOM011", "Invalid state" },
+	{ "DOM012", "Invalid node type" },
+	{ "DOM013", "Bad boundary points" }
     };
     /** Returns the list resource bundle contents. */
 
