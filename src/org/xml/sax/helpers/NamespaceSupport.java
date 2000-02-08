66677cb04fecfe1c26e8ea404ae98ce8582857e1
Andy Clark <andyc@apache.org>
2000-02-08 01:55:12 +0000

    Bug in SAX2beta helper class, NamespaceSupport, that made it depend on Java 2.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315261 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/xml/sax/helpers/NamespaceSupport.java
+++ src/org/xml/sax/helpers/NamespaceSupport.java
@@ -2,7 +2,7 @@
 // Written by David Megginson, sax@megginson.com
 // This class is in the Public Domain.  NO WARRANTY!
 
-// $Id: NamespaceSupport.java,v 1.3 2000/01/22 16:18:08 david Exp $
+// $Id: NamespaceSupport.java,v 1.1 2000/02/07 21:32:23 andyc Exp $
 
 package org.xml.sax.helpers;
 
@@ -522,7 +522,7 @@ public class NamespaceSupport
 	private void copyTables ()
 	{
 	    if (prefixTable != null) {
-		prefixTable = new Hashtable(prefixTable);
+		prefixTable = (Hashtable)prefixTable.clone();
 	    } else {
 		prefixTable = new Hashtable();
 	    }
