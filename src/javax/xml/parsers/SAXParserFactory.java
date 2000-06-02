dbe236a9caed71c0b62729578c6af68b7e7e9a02
Arnaud Le Hors <lehors@apache.org>
2000-06-02 23:42:21 +0000

    changed copyright to Apache for incorporation to Xerces, based on James Duncan authorization. See http://xml-archive.webweaving.org/xml-archive-xerces-j-dev/0545.html
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315729 13f79535-47bb-0310-9956-ffa450edef68

--- src/javax/xml/parsers/SAXParserFactory.java
+++ src/javax/xml/parsers/SAXParserFactory.java
@@ -1,32 +1,60 @@
-/******************************************************************************
- * Copyright (C) 1999-2000, Pierpaolo Fumagalli <mailto:pier@betaversion.org> *
- *                                                                            *
- * Redistribution and use in source  and/or in binary forms,  with or without *
- * modification are hereby permitted provided that the above copyright notice *
- * the following disclaimers, and this paragraph are not altered.             *
- *                                                                            *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, *
- * INCLUDING,  BUT NOT LIMITED TO,  THE IMPLIED WARRANTIES OF MERCHANTABILITY *
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE *
- * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, *
- * OR CONSEQUENTIAL DAMAGES  (INCLUDING,  BUT NOT LIMITED TO,  PROCUREMENT OF *
- * SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE, DATA, OR PROFITS;  OR BUSINESS *
- * INTERRUPTION)  HOWEVER CAUSED AND ON  ANY THEORY OF LIABILITY,  WHETHER IN *
- * CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE) *
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE *
- * POSSIBILITY OF SUCH DAMAGE.                                                *
- *                                                                            *
- * ATTENTION:                                                                 *
- *     THIS  IMPLEMENTATION  OF THE  "JAVAX.XML.PARSER"  CLASSES IS  PROVIDED *
- *     FOR  EXPERIMENTAL PURPOSES  ONLY  AND IS  NOT THE  OFFICIAL  REFERENCE *
- *     IMPLEMENTATION OF THE JAVA SPECIFICATION REQUEST 5 FOUND AT:           *
- *     <http://java.sun.com/aboutJava/communityprocess/jsr/jsr_005_xml.html>  *
- *                                                                            *
- *     THIS IMPLEMENTATION  IS CONFORMANT  TO THE  "JAVA API FOR XML PARSING" *
- *     SPECIFICATION  VERSION 1.0  PUBLIC RELEASE 1  BY JAMES DUNCAN DAVIDSON *
- *     ET AL. PUBLISHED BY SUN MICROSYSTEMS ON FEB. 18, 2000 AND FOUND AT:    *
- *     <http://java.sun.com/xml>                                              *
- ******************************************************************************/
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xerces" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999-2000, Pierpaolo
+ * Fumagalli <mailto:pier@betaversion.org>, http://www.apache.org.
+ * For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
 package javax.xml.parsers;
 
 import org.xml.sax.SAXException;
@@ -38,8 +66,8 @@ import org.xml.sax.SAXException;
  * <br>
  * <br>
  * <b>ATTENTION:</b> THIS IMPLEMENTATION OF THE "JAVAX.XML.PARSER" CLASSES
- *   IS PROVIDED FOR EXPERIMENTAL PURPOSES ONLY AND IS NOT THE OFFICIAL
- *   REFERENCE IMPLEMENTATION OF THE JAVA SPECIFICATION REQUEST 5 FOUND AT
+ *   IS NOT THE OFFICIAL REFERENCE IMPLEMENTATION OF THE JAVA SPECIFICATION
+ *   REQUEST 5 FOUND AT
  *   <a href="http://java.sun.com/aboutJava/communityprocess/jsr/jsr_005_xml.html">
  *   http://java.sun.com/aboutJava/communityprocess/jsr/jsr_005_xml.html
  *   </a><br>
@@ -61,8 +89,7 @@ import org.xml.sax.SAXException;
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @author <a href="pier@betaversion.org">Pierpaolo Fumagalli</a>
- * @author Copyright &copy; 1999-2000, Pierpaolo Fumagalli
- *         <a href="mailto:pier@betaversion.org">pier@betaversion.org</a>
+ * @author Copyright &copy; 2000 The Apache Software Foundation.
  * @version 1.0 CVS $Revision$ $Date$
  */
 public abstract class SAXParserFactory {
