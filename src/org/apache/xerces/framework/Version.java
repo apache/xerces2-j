9d50d245b8ca259243e2f02b18df77df1fba46b6
abagchi <dev-null@apache.org>
2000-06-02 18:01:13 +0000

    Corrected Version number that was accidentally overwritten
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315723 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces @@version@@";
+    public static String  fVersion = "Xerces 1.1.0";
 
     //
     // MAIN
