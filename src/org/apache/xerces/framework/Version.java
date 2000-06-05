a6668250540a4c707cbe059d27c1d0df2f32cff3
Jeff Rodriguez <jeffreyr@apache.org>
2000-06-05 17:07:28 +0000

    Release 1.1.1
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315737 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces 1.1.0";
+    public static String  fVersion = "Xerces 1.1.1";
 
     //
     // MAIN
