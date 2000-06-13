748093190cc7780cd5509be13a53edb326966f22
Andy Clark <andyc@apache.org>
2000-06-13 18:57:19 +0000

    String index out of bounds exception when scanning QName fixed.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315782 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/readers/StringReader.java
+++ src/org/apache/xerces/readers/StringReader.java
@@ -680,8 +680,11 @@ final class StringReader extends XMLEntityReader {
         int index = fData.indexOf(':', nameOffset);
         if (index != -1) {
             qname.prefix = fStringPool.addSymbol(fData.substring(nameOffset, index));
+            qname.localpart = fStringPool.addSymbol(fData.substring(index, fCurrentOffset));
         }
-        qname.localpart = fStringPool.addSymbol(fData.substring(index, fCurrentOffset));
+        else {
+            qname.localpart = qname.rawname;
+        }
 
     } // scanQName(char,QName)
 
