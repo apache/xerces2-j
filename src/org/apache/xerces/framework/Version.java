2555c6b3c5f60c5cf88899fe883d8a429d1ce511
Jeff Rodriguez <jeffreyr@apache.org>
2000-07-17 21:43:15 +0000

    change release number in prep for 1.1.3 release
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315927 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces 1.1.2";
+    public static String  fVersion = "Xerces 1.1.3";
 
     //
     // MAIN
