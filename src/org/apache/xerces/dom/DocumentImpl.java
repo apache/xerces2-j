29b7d02f68b049a199eb8e49efa19922e802c521
Jeff Rodriguez <jeffreyr@apache.org>
2000-08-08 15:32:30 +0000

    in xerces 1.1.3 DocumentImpl.importNode drops attribute values;
    an xml sample:
    
    <tag attr="value"> from an external dom
    becomes <tag attr=""> if imported.
    
    
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@315983 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/DocumentImpl.java
+++ src/org/apache/xerces/dom/DocumentImpl.java
@@ -793,6 +793,8 @@ public class DocumentImpl
                } else {
                    newnode = createAttribute(source.getNodeName());
                }
+
+               ((Attr)newnode).setValue( ((Attr)source).getValue() );
                 deep = true;
 		// Kids carry value
 		break;
