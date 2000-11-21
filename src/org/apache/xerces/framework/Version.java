01e6c33b543ec8e241e32ce5b217997a9e5f5479
Jeff Rodriguez <jeffreyr@apache.org>
2000-11-21 22:06:35 +0000

    next release Xerces-J_1_2_2
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@316605 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces 1.2.1";
+    public static String  fVersion = "Xerces 1.2.2";
 
     //
     // MAIN
