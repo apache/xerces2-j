7ba5db96e0ce09bb8517905d7c27b8301ee23d6d
Jeff Rodriguez <jeffreyr@apache.org>
2000-12-06 00:01:37 +0000

    Prepare files for Xerces 1.2.3
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@316689 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces 1.2.2";
+    public static String  fVersion = "Xerces 1.2.3";
 
     //
     // MAIN
