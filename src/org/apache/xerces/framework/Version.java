0371c4c76c806e24e4695f988559bae071535c0b
Neil Graham <neilg@apache.org>
2001-06-21 19:47:35 +0000

    changes for 1.4.1
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@317294 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/framework/Version.java
+++ src/org/apache/xerces/framework/Version.java
@@ -69,7 +69,7 @@ public class Version {
     //
 
     /** Version string. */
-    public static String  fVersion = "Xerces 1.4.0";
+    public static String  fVersion = "Xerces 1.4.1";
 
     //
     // MAIN
