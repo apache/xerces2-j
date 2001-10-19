3aa38daf17b9fe6681a1f36e87ea51f6b457a1e7
Andy Clark <andyc@apache.org>
2001-10-19 08:47:20 +0000

    Forgot to add implementation for isValidNCName method.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@317751 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/util/XMLChar.java
+++ src/org/apache/xerces/util/XMLChar.java
@@ -581,10 +581,20 @@ public class XMLChar {
      * @param name string to check
      * @return true if name is a valid NCName
      */
-    public static boolean isValidNCName(String name) {
-        //REVISIT: implement this method!!
+    public static boolean isValidNCName(String ncName) {
+        if (ncName.length() == 0)
+            return false;
+        char ch = ncName.charAt(0);
+        if( isNCNameStart(ch) == false)
+           return false;
+        for (int i = 1; i < ncName.length(); i++ ) {
+           ch = ncName.charAt(i);
+           if( isNCName( ch ) == false ){
+              return false;
+           }
+        }
         return true;
-    }
+    } // isValidNCName(String):boolean
 
     /*
      * [7] Nmtoken ::= (NameChar)+
