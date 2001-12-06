e09592488ba4d2b007a1ce314b62c72efd1898e6
Shudi Gao <sandygao@apache.org>
2001-12-06 23:22:40 +0000

    Instead of get the class name from a system property, we provide a method "setFactoryClass" to set which class name to use to create factory instance.
    Documentation is also updated.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@317976 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dv/SchemaDVFactory.java
+++ src/org/apache/xerces/impl/dv/SchemaDVFactory.java
@@ -57,14 +57,19 @@
 
 package org.apache.xerces.impl.dv;
 
-import org.apache.xerces.impl.dv.xs_new.SchemaDVFactoryImpl;
 import java.util.Hashtable;
 
 /**
- * The factory to create/return built-in schema simple types and create
- * user-defined simple types. The implementation should store the created
- * datatypes in static data, so that they can be shared by multiple parser
- * instance, and multiple threads.
+ * Defines a factory API that enables applications to <p>
+ * 1. to get the instance of specified SchemaDVFactory implementation <p>
+ * 2. to create/return built-in schema simple types <p>
+ * 3. to create user defined simple types. <p>
+ *
+ * Implementations of this abstract class can be used to get built-in simple
+ * types and create user-defined simle types. <p>
+ *
+ * The implementation should store the built-in datatypes in static data, so
+ * that they can be shared by multiple parser instance, and multiple threads.
  *
  * @author Sandy Gao, IBM
  *
@@ -72,62 +77,80 @@ import java.util.Hashtable;
  */
 public abstract class SchemaDVFactory {
 
-    /**
-     * property name of the schema dv factory implementation class
-     */
-    public static final String SCHEMA_DV_FACTORY_CLASS = "xerces.dv.schemadv";
+    private static final String DEFAULT_FACTORY_CLASS = "org.apache.xerces.impl.dv.xs_new.SchemaDVFactoryImpl";
 
-    private static SchemaDVFactory fSchemaDVFactory = null;
+    private static String          fFactoryClass    = null;
+    private static SchemaDVFactory fFactoryInstance = null;
+
+    /**
+     * Set the class name of the schema dv factory implementation. This method
+     * can only be called before the first time the method <code>getInstance</code>
+     * successfully returns, otherwise a DVFactoryException will be thrown.
+     *
+     * @param className  the class name of the SchemaDVFactory implementation
+     * @exception DVFactoryException  the method cannot be called at this time
+     */
+    public static final void setFactoryClass(String factoryClass) throws DVFactoryException {
+        // if the factory instance has been created, it's an error.
+        if (fFactoryInstance != null)
+            throw new DVFactoryException("Cannot set the class name now. The class name '"+fFactoryClass+"' is already used.");
+
+        // synchronize on the string value
+        synchronized (DEFAULT_FACTORY_CLASS) {
+            // in case this thread was waiting for another thread
+            if (fFactoryInstance != null)
+                throw new DVFactoryException("Cannot set the class name now. The class name '"+fFactoryClass+"' is already used.");
+
+            fFactoryClass = factoryClass;
+        }
+    }
 
     /**
      * Get an instance of SchemaDVFactory implementation.
      *
-     * The system property "xerces.dv.schemadv" needs to be specified to the
-     * class name of the implementation. If there is any error creating an
-     * instance of the specified class, the default implementation is used.
+     * If <code>setFactoryClass</code> is called before this method,
+     * the passed-in class name will be used to create the factory instance.
+     * Otherwise, a default implementation is used.
      *
      * After the first time this method successfully returns, any subsequent
-     * invocation to this method returns the same instance, even if the system
-     * property is changed later on.
+     * invocation to this method returns the same instance.
      *
      * @return  an instance of SchemaDVFactory implementation
+     * @exception DVFactoryException  cannot create an instance of the specified
+     *                                class name or the default class name
      */
-    public static final SchemaDVFactory getInstance() {
+    public static final SchemaDVFactory getInstance() throws DVFactoryException {
         // if the factory instance has been created, just return it.
-        if (fSchemaDVFactory != null)
-            return fSchemaDVFactory;
+        if (fFactoryInstance != null)
+            return fFactoryInstance;
 
         // synchronize on the string value, to make sure that we don't create
         // two instance of the dv factory class
-        synchronized (SCHEMA_DV_FACTORY_CLASS) {
+        synchronized (DEFAULT_FACTORY_CLASS) {
             // in case this thread was waiting for another thread to create
             // the factory instance, just return the instance created by the
             // other thread.
-            if (fSchemaDVFactory != null)
-                return fSchemaDVFactory;
-
-            // get the class name from the system property
-            String factoryClass = System.getProperty(SCHEMA_DV_FACTORY_CLASS);
+            if (fFactoryInstance != null)
+                return fFactoryInstance;
 
             try {
-                // if the class name is specified, try to create a new instance
-                // of that
-                if (factoryClass != null)
-                    fSchemaDVFactory = (SchemaDVFactory)(Class.forName(factoryClass).newInstance());
-            } catch (Exception e) {
-                // REVISIT: the system property specified by the application is
-                // not valid do we want to report some kind of error/warning?
+                // if the class name is not specified, use the default one
+                if (fFactoryClass == null)
+                    fFactoryClass = DEFAULT_FACTORY_CLASS;
+                fFactoryInstance = (SchemaDVFactory)(Class.forName(fFactoryClass).newInstance());
+            } catch (ClassNotFoundException e1) {
+                throw new DVFactoryException("Schema factory class " + fFactoryClass + " not found.");
+            } catch (IllegalAccessException e2) {
+                throw new DVFactoryException("Schema factory class " + fFactoryClass + " found but cannot be loaded.");
+            } catch (InstantiationException e3) {
+                throw new DVFactoryException("Schema factory class " + fFactoryClass + " loaded but cannot be instantiated (no empty public constructor?).");
+            } catch (ClassCastException e4) {
+                throw new DVFactoryException("Schema factory class " + fFactoryClass + " does not extend from SchemaDVFactory.");
             }
-
-            // if the class name is not specified, or we failed creating a new
-            // instance, create an instance of the default implementation of
-            // the dv factory interface
-            if (fSchemaDVFactory == null)
-                fSchemaDVFactory = new SchemaDVFactoryImpl();
         }
 
         // return the newly created dv factory instance
-        return fSchemaDVFactory;
+        return fFactoryInstance;
     }
 
     // can't create a new object of this class
@@ -165,7 +188,7 @@ public abstract class SchemaDVFactory {
      * @return                  the newly created simple type
      */
     public abstract XSSimpleType createTypeRestriction(String name, String targetNamespace,
-                                              short finalSet, XSSimpleType base);
+                                                       short finalSet, XSSimpleType base);
 
     /**
      * Create a new simple type which is derived by list from another simple
@@ -178,7 +201,7 @@ public abstract class SchemaDVFactory {
      * @return                  the newly created simple type
      */
     public abstract XSListSimpleType createTypeList(String name, String targetNamespace,
-                                           short finalSet, XSSimpleType itemType);
+                                                    short finalSet, XSSimpleType itemType);
 
     /**
      * Create a new simple type which is derived by union from a list of other
@@ -191,6 +214,6 @@ public abstract class SchemaDVFactory {
      * @return                  the newly created simple type
      */
     public abstract XSUnionSimpleType createTypeUnion(String name, String targetNamespace,
-                                             short finalSet, XSSimpleType[] memberTypes);
+                                                      short finalSet, XSSimpleType[] memberTypes);
 
 }
