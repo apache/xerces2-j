f9fa5e7d2704ffe4c0b275fbb32b49cd876ce66e
Shudi Gao <sandygao@apache.org>
2001-12-14 22:08:00 +0000

    1. XSDHandler is always expecting an entity resolver, so if the application didn't set one, we should use entity manager (the default implmenation) instead. 2. A check to prevent NPE.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@318019 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/parsers/DOMASBuilderImpl.java
+++ src/org/apache/xerces/parsers/DOMASBuilderImpl.java
@@ -2,7 +2,7 @@
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 2000,2001 The Apache Software Foundation.  All rights 
+ * Copyright (c) 2000,2001 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -18,7 +18,7 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
@@ -26,7 +26,7 @@
  *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache@apache.org.
  *
  * 5. Products derived from this software may not be called "Apache",
@@ -99,7 +99,7 @@ import org.apache.xerces.util.DOMUtil;
 
 
 /**
- * This is Abstract Schema DOM Builder class. It extends the DOMBuilderImpl 
+ * This is Abstract Schema DOM Builder class. It extends the DOMBuilderImpl
  * class. Provides support for preparsing schemas.
  *
  * @author Pavani Mukthipudi, Sun Microsystems Inc.
@@ -107,33 +107,35 @@ import org.apache.xerces.util.DOMUtil;
  * @version $Id$
  *
  */
- 
+
 public class DOMASBuilderImpl
     extends DOMBuilderImpl implements DOMASBuilder {
-    
+
     //
     // Constants
     //
-    
+
     // Feature ids
-    
-    protected static final String SCHEMA_FULL_CHECKING = 
+
+    protected static final String SCHEMA_FULL_CHECKING =
         Constants.XERCES_FEATURE_PREFIX + Constants.SCHEMA_FULL_CHECKING;
-        
+
     // Property ids
-    
-    protected static final String ERROR_REPORTER = 
+
+    protected static final String ERROR_REPORTER =
         Constants.XERCES_PROPERTY_PREFIX + Constants.ERROR_REPORTER_PROPERTY;
-        
+
     protected static final String SYMBOL_TABLE =
         Constants.XERCES_PROPERTY_PREFIX + Constants.SYMBOL_TABLE_PROPERTY;
-        
-    
-    
+
+    protected static final String ENTITY_MANAGER =
+        Constants.XERCES_PROPERTY_PREFIX + Constants.ENTITY_MANAGER_PROPERTY;
+
+
     //
     // Data
     //
-    
+
     protected XSGrammarResolver fGrammarResolver;
     protected SubstitutionGroupHandler fSubGroupHandler;
     protected CMBuilder fCMBuilder;
@@ -142,7 +144,7 @@ public class DOMASBuilderImpl
     protected XMLEntityResolver fEntityResolver;
     protected SymbolTable fSymbolTable;
     protected GrammarPool fGrammarPool = null;
-    
+
     protected ASModelImpl fAbstractSchema;
 
     //
@@ -183,27 +185,27 @@ public class DOMASBuilderImpl
     //
     // DOMASBuilder methods
     //
-        
+
     /**
-     * Associate an <code>ASModel</code> with a document instance. This 
+     * Associate an <code>ASModel</code> with a document instance. This
      * <code>ASModel</code> will be used by the "
      * <code>validate-if-schema</code>" and "
-     * <code>datatype-normalization</code>" options during the load of a new 
-     * <code>Document</code>. 
+     * <code>datatype-normalization</code>" options during the load of a new
+     * <code>Document</code>.
      */
     public ASModel getAbstractSchema() {
-    	return fAbstractSchema;
+        return fAbstractSchema;
     }
-    
+
     /**
-     * Associate an <code>ASModel</code> with a document instance. This 
+     * Associate an <code>ASModel</code> with a document instance. This
      * <code>ASModel</code> will be used by the "
      * <code>validate-if-schema</code>" and "
-     * <code>datatype-normalization</code>" options during the load of a new 
-     * <code>Document</code>. 
+     * <code>datatype-normalization</code>" options during the load of a new
+     * <code>Document</code>.
      */
     public void setAbstractSchema(ASModel abstractSchema) {
-        
+
         // since the ASModel associated with this object is an attribute
         // according to the DOM IDL, we must obliterate anything
         // that was set before, rather than adding to it.
@@ -215,8 +217,8 @@ public class DOMASBuilderImpl
         }
         initGrammarPool(fAbstractSchema);
     }
-    
-    
+
+
     // some constants that'll be added into the symbol table
     String XMLNS;
     String URI_XSI;
@@ -225,29 +227,29 @@ public class DOMASBuilderImpl
     String XSI_TYPE;
     String XSI_NIL;
     String URI_SCHEMAFORSCHEMA;
-    
+
     /**
      * Parse a Abstract Schema from a location identified by an URI.
-     * 
-     * @param uri The location of the Abstract Schema to be read. 
+     *
+     * @param uri The location of the Abstract Schema to be read.
      * @return The newly created <code>Abstract Schema</code>.
      * @exception DOMASException
-     *   Exceptions raised by <code>parseASURI()</code> originate with the 
-     *   installed ErrorHandler, and thus depend on the implementation of 
-     *   the <code>DOMErrorHandler</code> interfaces. The default error 
-     *   handlers will raise a <code>DOMASException</code> if any form of 
-     *   Abstract Schema inconsistencies or warning occurs during the parse, 
-     *   but application defined errorHandlers are not required to do so. 
-     *   <br> WRONG_MIME_TYPE_ERR: Raised when <code>mimeTypeCheck</code> is 
-     *   <code>true</code> and the inputsource has an incorrect MIME Type. 
-     *   See attribute <code>mimeTypeCheck</code>. 
+     *   Exceptions raised by <code>parseASURI()</code> originate with the
+     *   installed ErrorHandler, and thus depend on the implementation of
+     *   the <code>DOMErrorHandler</code> interfaces. The default error
+     *   handlers will raise a <code>DOMASException</code> if any form of
+     *   Abstract Schema inconsistencies or warning occurs during the parse,
+     *   but application defined errorHandlers are not required to do so.
+     *   <br> WRONG_MIME_TYPE_ERR: Raised when <code>mimeTypeCheck</code> is
+     *   <code>true</code> and the inputsource has an incorrect MIME Type.
+     *   See attribute <code>mimeTypeCheck</code>.
      * @exception DOMSystemException
-     *   Exceptions raised by <code>parseURI()</code> originate with the 
-     *   installed ErrorHandler, and thus depend on the implementation of 
-     *   the <code>DOMErrorHandler</code> interfaces. The default error 
-     *   handlers will raise a DOMSystemException if any form I/O or other 
-     *   system error occurs during the parse, but application defined error 
-     *   handlers are not required to do so. 
+     *   Exceptions raised by <code>parseURI()</code> originate with the
+     *   installed ErrorHandler, and thus depend on the implementation of
+     *   the <code>DOMErrorHandler</code> interfaces. The default error
+     *   handlers will raise a DOMSystemException if any form I/O or other
+     *   system error occurs during the parse, but application defined error
+     *   handlers are not required to do so.
      */
     public ASModel parseASURI(String uri)
                               throws DOMASException, Exception {
@@ -257,16 +259,18 @@ public class DOMASBuilderImpl
            fSchemaHandler = new XSDHandler(fGrammarResolver);
            fCMBuilder = new CMBuilder(new XSDeclarationPool());
        }
-       
+
        fErrorReporter = (XMLErrorReporter)fConfiguration.getProperty(ERROR_REPORTER);
        fEntityResolver = (XMLEntityResolver)fConfiguration.getProperty(ENTITY_RESOLVER);
-       
+       if (fEntityResolver == null)
+           fEntityResolver = (XMLEntityResolver)fConfiguration.getProperty(ENTITY_MANAGER);
+
        fSymbolTable = (SymbolTable)fConfiguration.getProperty(SYMBOL_TABLE);
-       String externalSchemas = 
+       String externalSchemas =
             (String)(fConfiguration.getProperty(Constants.XERCES_PROPERTY_PREFIX+Constants.SCHEMA_LOCATION));
-       String noNamespaceExternalSchemas = 
+       String noNamespaceExternalSchemas =
             (String)(fConfiguration.getProperty(Constants.XERCES_PROPERTY_PREFIX+Constants.SCHEMA_NONS_LOCATION));
-       
+
        XMLNS = fSymbolTable.addSymbol(SchemaSymbols.O_XMLNS);
        URI_XSI = fSymbolTable.addSymbol(SchemaSymbols.URI_XSI);
        XSI_SCHEMALOCATION = fSymbolTable.addSymbol(SchemaSymbols.OXSI_SCHEMALOCATION);
@@ -274,13 +278,13 @@ public class DOMASBuilderImpl
        XSI_TYPE = fSymbolTable.addSymbol(SchemaSymbols.OXSI_TYPE);
        XSI_NIL = fSymbolTable.addSymbol(SchemaSymbols.OXSI_NIL);
        URI_SCHEMAFORSCHEMA = fSymbolTable.addSymbol(SchemaSymbols.OURI_SCHEMAFORSCHEMA);
-       
+
        initGrammarResolver();
        fSubGroupHandler.reset();
        fSchemaHandler.reset(fErrorReporter, fEntityResolver, fSymbolTable, externalSchemas, noNamespaceExternalSchemas);
-       
+
        SchemaGrammar grammar = fSchemaHandler.parseSchema(null,uri);
-       
+
        if (getFeature(SCHEMA_FULL_CHECKING)) {
            XSConstraints.fullSchemaChecking(fGrammarResolver, fSubGroupHandler, fCMBuilder, fErrorReporter);
        }
@@ -291,29 +295,29 @@ public class DOMASBuilderImpl
     }
 
     /**
-     * Parse a Abstract Schema from a location identified by an 
+     * Parse a Abstract Schema from a location identified by an
      * <code>DOMInputSource</code>.
-     * 
-     * @param is The <code>DOMInputSource</code> from which the source 
-     *   Abstract Schema is to be read. 
+     *
+     * @param is The <code>DOMInputSource</code> from which the source
+     *   Abstract Schema is to be read.
      * @return The newly created <code>ASModel</code>.
      * @exception DOMASException
-     *   Exceptions raised by <code>parseASURI()</code> originate with the 
-     *   installed ErrorHandler, and thus depend on the implementation of 
-     *   the <code>DOMErrorHandler</code> interfaces. The default error 
-     *   handlers will raise a <code>DOMASException</code> if any form of 
-     *   Abstract Schema inconsistencies or warning occurs during the parse, 
-     *   but application defined errorHandlers are not required to do so. 
-     *   <br> WRONG_MIME_TYPE_ERR: Raised when <code>mimeTypeCheck</code> is 
-     *   true and the inputsource has an incorrect MIME Type. See attribute 
-     *   <code>mimeTypeCheck</code>. 
+     *   Exceptions raised by <code>parseASURI()</code> originate with the
+     *   installed ErrorHandler, and thus depend on the implementation of
+     *   the <code>DOMErrorHandler</code> interfaces. The default error
+     *   handlers will raise a <code>DOMASException</code> if any form of
+     *   Abstract Schema inconsistencies or warning occurs during the parse,
+     *   but application defined errorHandlers are not required to do so.
+     *   <br> WRONG_MIME_TYPE_ERR: Raised when <code>mimeTypeCheck</code> is
+     *   true and the inputsource has an incorrect MIME Type. See attribute
+     *   <code>mimeTypeCheck</code>.
      * @exception DOMSystemException
-     *   Exceptions raised by <code>parseURI()</code> originate with the 
-     *   installed ErrorHandler, and thus depend on the implementation of 
-     *   the <code>DOMErrorHandler</code> interfaces. The default error 
-     *   handlers will raise a DOMSystemException if any form I/O or other 
-     *   system error occurs during the parse, but application defined error 
-     *   handlers are not required to do so. 
+     *   Exceptions raised by <code>parseURI()</code> originate with the
+     *   installed ErrorHandler, and thus depend on the implementation of
+     *   the <code>DOMErrorHandler</code> interfaces. The default error
+     *   handlers will raise a DOMSystemException if any form I/O or other
+     *   system error occurs during the parse, but application defined error
+     *   handlers are not required to do so.
      */
     public ASModel parseASInputSource(DOMInputSource is)
                                       throws DOMASException, Exception {
@@ -323,16 +327,16 @@ public class DOMASBuilderImpl
            fSchemaHandler = new XSDHandler(fGrammarResolver);
            fCMBuilder = new CMBuilder(new XSDeclarationPool());
        }
-       
+
        fErrorReporter = (XMLErrorReporter)fConfiguration.getProperty(ERROR_REPORTER);
        fEntityResolver = (XMLEntityResolver)fConfiguration.getProperty(ENTITY_RESOLVER);
-       
+
        fSymbolTable = (SymbolTable)fConfiguration.getProperty(SYMBOL_TABLE);
-       String externalSchemas = 
+       String externalSchemas =
             (String)(fConfiguration.getProperty(Constants.XERCES_PROPERTY_PREFIX+Constants.SCHEMA_LOCATION));
-       String noNamespaceExternalSchemas = 
+       String noNamespaceExternalSchemas =
             (String)(fConfiguration.getProperty(Constants.XERCES_PROPERTY_PREFIX+Constants.SCHEMA_NONS_LOCATION));
-       
+
        XMLNS = fSymbolTable.addSymbol(SchemaSymbols.O_XMLNS);
        URI_XSI = fSymbolTable.addSymbol(SchemaSymbols.URI_XSI);
        XSI_SCHEMALOCATION = fSymbolTable.addSymbol(SchemaSymbols.OXSI_SCHEMALOCATION);
@@ -340,7 +344,7 @@ public class DOMASBuilderImpl
        XSI_TYPE = fSymbolTable.addSymbol(SchemaSymbols.OXSI_TYPE);
        XSI_NIL = fSymbolTable.addSymbol(SchemaSymbols.OXSI_NIL);
        URI_SCHEMAFORSCHEMA = fSymbolTable.addSymbol(SchemaSymbols.OURI_SCHEMAFORSCHEMA);
-              
+
        initGrammarResolver();
        fSubGroupHandler.reset();
        fSchemaHandler.reset(fErrorReporter, fEntityResolver, fSymbolTable, externalSchemas, noNamespaceExternalSchemas);
@@ -355,12 +359,13 @@ public class DOMASBuilderImpl
        addGrammars(newAsModel, fGrammarResolver);
        return newAsModel;
     }
-    
+
     // put all the grammars we have access to in the GrammarResolver
     private void initGrammarResolver() {
         fGrammarResolver.reset();
         fGrammarResolver.putGrammar(URI_SCHEMAFORSCHEMA, SchemaGrammar.SG_SchemaNS);
-        initGrammarResolverRecurse(fAbstractSchema);
+        if (fAbstractSchema != null)
+            initGrammarResolverRecurse(fAbstractSchema);
     }
     private void initGrammarResolverRecurse(ASModelImpl currModel) {
         if(currModel.getGrammar() != null) {
@@ -368,7 +373,7 @@ public class DOMASBuilderImpl
         }
         for(int i = 0; i < currModel.getInternalASModels().size(); i++) {
             ASModelImpl nextModel = (ASModelImpl)(currModel.getInternalASModels().elementAt(i));
-            initGrammarResolverRecurse(nextModel); 
+            initGrammarResolverRecurse(nextModel);
         }
     }
 
@@ -378,10 +383,10 @@ public class DOMASBuilderImpl
             ASModelImpl newModel = new ASModelImpl();
             newModel.setGrammar(grammarList[i]);
             model.addASModel(newModel);
-        } 
+        }
     } // addGrammars
 
-    private void initGrammarPool(ASModelImpl currModel) { 
+    private void initGrammarPool(ASModelImpl currModel) {
         // put all the grammars in fAbstractSchema into the grammar pool.
         SchemaGrammar grammar = null;
         if((grammar = currModel.getGrammar()) != null) {
