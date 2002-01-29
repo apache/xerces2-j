925a49f1bb1418723537dce4c3d24344f0ff29c1
Arnaud Le Hors <lehors@apache.org>
2002-01-29 01:15:21 +0000

    updated copyright
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@318163 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/DOMInputSourceImpl.java
+++ src/org/apache/xerces/dom/DOMInputSourceImpl.java
@@ -2,7 +2,7 @@
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 2001 The Apache Software Foundation.  
+ * Copyright (c) 2001, 2002 The Apache Software Foundation.  
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
