68617f2f7f8e9f26ab453263a69373171a261c72
Shudi Gao <sandygao@apache.org>
2002-06-04 16:51:14 +0000

    A performance patch from Henry Zongaro: "In most cases, the Hashtable that's used for these Augmentations objects contains only a couple of entries, so I thought that it might be better to have the AugmentationsImpl contain two different inner classes to store the augmentations.  When there are only a few augmentations, the AugmentationsImpl uses a class that contains an array that contains the keys and values; when the number of augmentations exceeds a certain value, it switches to the other inner class which uses a Hashtable. I arbitrarily chose 10, but didn't experiment to find the break-even point."
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@318435 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/util/AugmentationsImpl.java
+++ src/org/apache/xerces/util/AugmentationsImpl.java
@@ -74,7 +74,8 @@ import org.apache.xerces.xni.Augmentations;
  */
 public class AugmentationsImpl implements Augmentations{
     
-    private final Hashtable fAugmentations = new Hashtable();
+    private AugmentationsItemsContainer fAugmentationsContainer =
+                                        new SmallContainer();
     
     /**
      * Add additional information identified by a key to the Augmentations structure.
@@ -86,8 +87,13 @@ public class AugmentationsImpl implements Augmentations{
      *         or <code>null</code> if it did not have one.
      */
     public Object putItem (String key, Object item){
-        return fAugmentations.put(key, item);
+        Object oldValue = fAugmentationsContainer.putItem(key, item);
 
+        if (oldValue == null && fAugmentationsContainer.isFull()) {
+            fAugmentationsContainer = fAugmentationsContainer.expand();
+        }
+
+        return oldValue;
     }
 
 
@@ -100,7 +106,7 @@ public class AugmentationsImpl implements Augmentations{
      *         <code>null</code> if the key is not mapped to any value.
      */
     public Object getItem(String key){
-        return fAugmentations.get(key);
+        return fAugmentationsContainer.getItem(key);
     }
     
     
@@ -110,7 +116,7 @@ public class AugmentationsImpl implements Augmentations{
      * @param key    Identifier, can't be <code>null</code>
      */
     public Object removeItem (String key){
-        return fAugmentations.remove(key);
+        return fAugmentationsContainer.removeItem(key);
     }
 
     /**
@@ -118,13 +124,203 @@ public class AugmentationsImpl implements Augmentations{
      *
      */
     public Enumeration keys (){
-        return fAugmentations.keys();
+        return fAugmentationsContainer.keys();
     }
 
     /**
      * Remove all objects from the Augmentations structure.
      */
     public void clear (){
-        fAugmentations.clear();
+        fAugmentationsContainer.clear();
+    }
+
+    public String toString() {
+        return fAugmentationsContainer.toString();
+    }
+
+    abstract class AugmentationsItemsContainer {
+        abstract public Object putItem(Object key, Object item);
+        abstract public Object getItem(Object key);
+        abstract public Object removeItem(Object key);
+        abstract public Enumeration keys();
+        abstract public void clear();
+        abstract public boolean isFull();
+        abstract public AugmentationsItemsContainer expand();
+    }
+
+    class SmallContainer extends AugmentationsItemsContainer {
+        final static int SIZE_LIMIT = 10;
+        final Object[] fAugmentations = new Object[SIZE_LIMIT*2];
+        int fNumEntries = 0;
+
+        public Enumeration keys() {
+            return new SmallContainerKeyEnumeration();
+        }
+
+        public Object getItem(Object key) {
+            for (int i = 0; i < fNumEntries*2; i = i + 2) {
+                if (fAugmentations[i].equals(key)) {
+                    return fAugmentations[i+1];
+                }
+            }
+
+            return null;
+        }
+
+        public Object putItem(Object key, Object item) {
+            for (int i = 0; i < fNumEntries*2; i = i + 2) {
+                if (fAugmentations[i].equals(key)) {
+                    Object oldValue = fAugmentations[i+1];
+                    fAugmentations[i+1] = item;
+
+                    return oldValue;
+                }
+            }
+
+            fAugmentations[fNumEntries*2] = key;
+            fAugmentations[fNumEntries*2+1] = item;
+            fNumEntries++;
+
+            return null;
+        }
+
+
+        public Object removeItem(Object key) {
+            for (int i = 0; i < fNumEntries*2; i = i + 2) {
+                if (fAugmentations[i].equals(key)) {
+                    Object oldValue = fAugmentations[i+1];
+
+                    for (int j = i; j < fNumEntries*2 - 2; j = j + 2) {
+                        fAugmentations[j] = fAugmentations[j+2];
+                        fAugmentations[j+1] = fAugmentations[j+3];
+                    }
+
+                    fAugmentations[fNumEntries*2-2] = null;
+                    fAugmentations[fNumEntries*2-1] = null;
+                    fNumEntries--;
+
+                    return oldValue;
+                }
+            }
+
+            return null;
+        }
+
+        public void clear() {
+            for (int i = 0; i < fNumEntries*2; i = i + 2) {
+                fAugmentations[i] = null;
+                fAugmentations[i+1] = null;
+            }
+
+            fNumEntries = 0;
+        }
+
+        public boolean isFull() {
+            return (fNumEntries == SIZE_LIMIT);
+        }
+
+        public AugmentationsItemsContainer expand() {
+            LargeContainer expandedContainer = new LargeContainer();
+
+            for (int i = 0; i < fNumEntries*2; i = i + 2) {
+                expandedContainer.putItem(fAugmentations[i],
+                                          fAugmentations[i+1]);
+            }
+
+            return expandedContainer;
+        }
+
+        public String toString() {
+            StringBuffer buff = new StringBuffer();
+            buff.append("SmallContainer - fNumEntries == " + fNumEntries);
+
+            for (int i = 0; i < SIZE_LIMIT*2; i=i+2) {
+                buff.append("\nfAugmentations[");
+                buff.append(i);
+                buff.append("] == ");
+                buff.append(fAugmentations[i]);
+                buff.append("; fAugmentations[");
+                buff.append(i+1);
+                buff.append("] == ");
+                buff.append(fAugmentations[i+1]);
+            }
+
+            return buff.toString();
+        }
+
+        class SmallContainerKeyEnumeration implements Enumeration {
+            Object [] enumArray = new Object[fNumEntries];
+            int next = 0;
+
+            SmallContainerKeyEnumeration() {
+                for (int i = 0; i < fNumEntries; i++) {
+                    enumArray[i] = fAugmentations[i*2];
+                }
+            }
+
+            public boolean hasMoreElements() {
+                return next < enumArray.length;
+            }
+
+            public Object nextElement() {
+                if (next >= enumArray.length) {
+                    throw new java.util.NoSuchElementException();
+                }
+
+                Object nextVal = enumArray[next];
+                enumArray[next] = null;
+                next++;
+
+                return nextVal;
+            }
+        }
+    }
+
+    class LargeContainer extends AugmentationsItemsContainer {
+        final Hashtable fAugmentations = new Hashtable();
+
+        public Object getItem(Object key) {
+            return fAugmentations.get(key);
+        }
+
+        public Object putItem(Object key, Object item) {
+            return fAugmentations.put(key, item);
+        }
+
+        public Object removeItem(Object key) {
+            return fAugmentations.remove(key);
+        }
+
+        public Enumeration keys() {
+            return fAugmentations.keys();
+        }
+
+        public void clear() {
+            fAugmentations.clear();
+        }
+
+        public boolean isFull() {
+            return false;
+        }
+
+        public AugmentationsItemsContainer expand() {
+            return this;
+        }
+
+        public String toString() {
+            StringBuffer buff = new StringBuffer();
+            buff.append("LargeContainer");
+            Enumeration enum = fAugmentations.keys();
+
+            while (enum.hasMoreElements()) {
+                Object key = enum.nextElement();
+                buff.append("\nkey == ");
+                buff.append(key);
+                buff.append("; value == ");
+                buff.append(fAugmentations.get(key));
+            }
+
+            return buff.toString();
+        }
     }
 }
