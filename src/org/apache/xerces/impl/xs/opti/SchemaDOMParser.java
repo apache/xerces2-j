4b5fda8bebeabccb0320597e468197a52b089f60
Rahul Srivastava <rahuls@apache.org>
2002-08-14 09:59:51 +0000

    Loading schema as a DTM instead of DOM. Using this we get ~50% improvement in schema loading and a overall of ~9-11% improvement.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/jaxp-ri-1_2_0-fcs-branch@318619 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xerces/impl/xs/opti/SchemaDOMParser.java
@@ -0,0 +1,283 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2001, 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xerces" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, International
+ * Business Machines, Inc., http://www.apache.org.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.xerces.impl.xs.opti;
+
+
+import org.apache.xerces.xni.QName;
+import org.apache.xerces.xni.XMLString;
+import org.apache.xerces.xni.XMLAttributes;
+import org.apache.xerces.xni.Augmentations;
+import org.apache.xerces.xni.XMLDocumentHandler;
+import org.apache.xerces.xni.parser.XMLComponent;
+import org.apache.xerces.xni.parser.XMLComponentManager;
+import org.apache.xerces.xni.parser.XMLDocumentSource;
+
+import org.apache.xerces.xni.XNIException;
+import org.apache.xerces.xni.parser.XMLConfigurationException;
+
+import org.w3c.dom.Document;
+
+
+/**
+ * @author Rahul Srivastava, Sun Microsystems Inc.
+ *
+ * @version $Id$
+ */
+public class SchemaDOMParser extends DefaultXMLDocumentHandler
+                             implements XMLComponent, XMLDocumentSource {
+
+    //
+    // Data
+    //
+    
+    SchemaDOM schemaDOM;
+    
+    /** Document handler. */
+    protected XMLDocumentHandler fDocumentHandler;
+    
+
+    //
+    // Constructors
+    //
+
+    /** Default constructor. */
+    public SchemaDOMParser() {
+    }
+
+
+    //
+    // XMLComponent methods
+    //
+
+    /**
+     * Resets the component. The component can query the component manager
+     * about any features and properties that affect the operation of the
+     * component.
+     *
+     * @param componentManager The component manager.
+     *
+     * @throws SAXException Thrown by component on initialization error.
+     *                      For example, if a feature or property is
+     *                      required for the operation of the component, the
+     *                      component manager may throw a
+     *                      SAXNotRecognizedException or a
+     *                      SAXNotSupportedException.
+     */
+    public void reset(XMLComponentManager componentManager)
+        throws XNIException {
+
+        schemaDOM = new SchemaDOM();
+
+    } // reset(XMLComponentManager)
+
+    /**
+     * Returns a list of feature identifiers that are recognized by
+     * this component. This method may return null if no features
+     * are recognized by this component.
+     */
+    public String[] getRecognizedFeatures() {
+        return null;
+    } // getRecognizedFeatures():String[]
+
+    /**
+     * Sets the state of a feature. This method is called by the component
+     * manager any time after reset when a feature changes state.
+     * <p>
+     * <strong>Note:</strong> Components should silently ignore features
+     * that do not affect the operation of the component.
+     *
+     * @param featureId The feature identifier.
+     * @param state     The state of the feature.
+     *
+     * @throws SAXNotRecognizedException The component should not throw
+     *                                   this exception.
+     * @throws SAXNotSupportedException The component should not throw
+     *                                  this exception.
+     */
+    public void setFeature(String featureId, boolean state)
+        throws XMLConfigurationException {
+    } // setFeature(String,boolean)
+
+    /**
+     * Returns a list of property identifiers that are recognized by
+     * this component. This method may return null if no properties
+     * are recognized by this component.
+     */
+    public String[] getRecognizedProperties() {
+        return null;
+    } // getRecognizedProperties():String[]
+
+    /**
+     * Sets the value of a property during parsing.
+     *
+     * @param propertyId
+     * @param value
+     */
+    public void setProperty(String propertyId, Object value)
+        throws XMLConfigurationException {
+
+    } // setProperty(String,Object)
+
+
+    //
+    // XMLDocumentSource methods
+    //
+
+    /**
+     * Sets the document handler to receive information about the document.
+     *
+     * @param documentHandler The document handler.
+     */
+    public void setDocumentHandler(XMLDocumentHandler documentHandler) {
+        fDocumentHandler = documentHandler;
+    } // setDocumentHandler(XMLDocumentHandler)
+
+    //
+    // XMLDocumentHandler methods
+    //
+
+    /**
+     * The end of the document.
+     * @param augs     Additional information that may include infoset augmentations
+     *
+     * @throws XNIException Thrown by handler to signal an error.
+     */
+    public void endDocument(Augmentations augs) throws XNIException {
+	// To debug the DOM created uncomment the line below
+	// schemaDOM.printDOM();
+    } // endDocument()
+
+
+    /**
+     * Character content.
+     * 
+     * @param text   The content.
+     * @param augs   Additional information that may include infoset augmentations
+     *               
+     * @exception XNIException
+     *                   Thrown by handler to signal an error.
+     */
+    public void characters(XMLString text, Augmentations augs) throws XNIException {
+	schemaDOM.characters(text, augs);
+    }
+
+
+    /**
+     * The start of an element.
+     * 
+     * @param element    The name of the element.
+     * @param attributes The element attributes.
+     * @param augs       Additional information that may include infoset augmentations
+     *                   
+     * @exception XNIException
+     *                   Thrown by handler to signal an error.
+     */
+    public void startElement(QName element, XMLAttributes attributes, Augmentations augs)
+        throws XNIException {
+
+	schemaDOM.startElement(element, attributes, augs);
+
+    }
+
+
+    /**
+     * An empty element.
+     * 
+     * @param element    The name of the element.
+     * @param attributes The element attributes.
+     * @param augs       Additional information that may include infoset augmentations
+     *                   
+     * @exception XNIException
+     *                   Thrown by handler to signal an error.
+     */
+    public void emptyElement(QName element, XMLAttributes attributes, Augmentations augs)
+        throws XNIException {
+
+	schemaDOM.emptyElement(element, attributes, augs);
+
+    }
+
+
+    /**
+     * The end of an element.
+     * 
+     * @param element The name of the element.
+     * @param augs    Additional information that may include infoset augmentations
+     *                
+     * @exception XNIException
+     *                   Thrown by handler to signal an error.
+     */
+    public void endElement(QName element, Augmentations augs) throws XNIException {
+
+	schemaDOM.endElement(element,  augs);
+
+    }
+    
+    
+    //
+    // other methods
+    //
+    
+    /**
+     * Returns the DOM document object.
+     */
+    public Document getDocument() {
+        return schemaDOM;
+    }
+
+}
