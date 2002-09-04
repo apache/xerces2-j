3bcc2d9d464160d39a3ba7973706079e24a3daad
Elena Litani <elena@apache.org>
2002-09-04 19:02:05 +0000

    Patch: the current version in the Apache repository does not support request such as "XML 2.0 LS-Load 3.0". Submitted by: Philippe Le Hegaret Applied by: Elena Litani
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@318689 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/DOMImplementationSourceImpl.java
+++ src/org/apache/xerces/dom/DOMImplementationSourceImpl.java
@@ -108,12 +108,43 @@ public class DOMImplementationSourceImpl
     }
 
     boolean testImpl(DOMImplementation impl, String features) {
+       
         StringTokenizer st = new StringTokenizer(features);
-        while (st.hasMoreTokens()) {
-            String feature = st.nextToken();
-            if (!impl.hasFeature(feature, null)) {
-                return false;
-            }
+        String feature = null;
+        String version = null;
+ 
+        if (st.hasMoreTokens()) {
+           feature = st.nextToken();
+        }
+        while (feature != null) {
+           boolean isVersion = false;
+           if (st.hasMoreTokens()) {
+               char c;
+               version = st.nextToken();
+               c = version.charAt(0);
+               switch (c) {
+               case '0': case '1': case '2': case '3': case '4':
+               case '5': case '6': case '7': case '8': case '9':
+                   isVersion = true;
+               }
+           } else {
+               version = null;
+           }
+           if (isVersion) {
+               if (!impl.hasFeature(feature, version)) {
+                   return false;
+               }
+               if (st.hasMoreTokens()) {
+                   feature = st.nextToken();
+               } else {
+                   feature = null;
+               }
+           } else {
+               if (!impl.hasFeature(feature, null)) {
+                   return false;
+               }
+               feature = version;
+           }
         }
         return true;
     }
