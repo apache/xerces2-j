8c22cec9f49d30156123c1d942643b75fbfecb87
Elena Litani <elena@apache.org>
2002-12-30 21:33:04 +0000

    Fix tests
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@318903 13f79535-47bb-0310-9956-ffa450edef68

--- tests/dom/dom3/Test.java
+++ tests/dom/dom3/Test.java
@@ -246,6 +246,7 @@ public class Test implements DOMErrorHandler, DOMEntityResolver{
                 config.setParameter("error-handler",errorHandler);
                 config.setParameter("validate", Boolean.TRUE);
                 config.setParameter("psvi", Boolean.TRUE);
+                config.setParameter("discard-default-content", Boolean.FALSE);
                 DocumentImpl core = (DocumentImpl)builder.parseURI("data/personal-schema.xml");
                 Assertion.assert(errorCounter == 0, "No errors should be reported");
 
