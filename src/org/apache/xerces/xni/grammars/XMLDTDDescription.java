45f5a513165a0b933a33bb8764f5aeb617889d69
Shudi Gao <sandygao@apache.org>
2003-01-07 22:46:15 +0000

    Two new grammar description interfaces, for describing DTD and XML Schema grammars respectively. With these interfaces, users can get more detailed information about a grammar.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@318925 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/xni/grammars/XMLDTDDescription.java
+++ src/org/apache/xerces/xni/grammars/XMLDTDDescription.java
@@ -2,7 +2,7 @@
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 2000-2002 The Apache Software Foundation.  All rights 
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights 
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -57,41 +57,19 @@
 
 package org.apache.xerces.xni.grammars;
 
-import org.apache.xerces.xni.XMLResourceIdentifier;
-
 /*
- * <p> This interface describes basic attributes of XML grammars--their
- * physical location and their type. </p>
- *
- * @author Neil Graham, IBM
+ * All information specific to DTD grammars.  
+ * 
+ * @author Sandy Gao, IBM
  * @version $Id$
  */
-public interface XMLGrammarDescription extends XMLResourceIdentifier {
-
-    // initial set of grammar constants that some configurations will recognize;user
-    // components which create and/or recognize other types of grammars may
-    // certainly use their own constants in place of these (so long as
-    // their Grammar objects implement this interface).
+public interface XMLDTDDescription extends XMLGrammarDescription {
 
     /**
-     * The grammar type constant for XML Schema grammars. When getGrammarType()
-     * method returns this constant, the object should be an instance of
-     * the XMLSchemaDescription interface.
-     */
-    public static final String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
-    
-    /**
-     * The grammar type constant for DTD grammars. When getGrammarType()
-     * method returns this constant, the object should be an instance of
-     * the XMLDTDDescription interface.
-     */
-    public static final String XML_DTD = "http://www.w3.org/TR/REC-xml";
-
-    /**
-     * Return the type of this grammar.
+     * Return the root name of this DTD.
      * 
-     * @return  the type of this grammar
+     * @return  the root name. null if the name is unknown.
      */
-    public String getGrammarType();
+    public String getRootName();
 
-} // interface XMLGrammarDescription
+} // interface XMLDTDDescription
