f9508d7416156894484392c161093dc15d4fbadd
Shudi Gao <sandygao@apache.org>
2003-01-23 23:12:27 +0000

    Making the behavior of dynamic validation consistent: - Make it work as what the feature description says; - Make DTD and Schema validation work the same way. That is, when dynamic validation feature is on, dynamic validation always happens, in spite of the state of the validation feature.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@319012 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dtd/XMLDTDValidator.java
+++ src/org/apache/xerces/impl/dtd/XMLDTDValidator.java
@@ -1076,9 +1076,21 @@ public class XMLDTDValidator
     }
     
     public final boolean validate(){
+        // Do validation if all of the following are true:
+        // 1. The JAXP Schema Language property is not XML Schema
+        //    REVISIT: since only DTD and Schema are supported at this time,
+        //             such checking is sufficient. but if more schema types
+        //             are introduced in the future, we'll need to change it
+        //             to something like
+        //             (fSchemaType == null || fSchemaType == NS_XML_DTD)
+        // 2. One of the following is true (validation features)
+        // 2.1 Dynamic validation is off, and validation is on
+        // 2.2 Dynamic validation is on, and DOCTYPE was seen
+        // 3 Xerces schema validation feature is off, or DOCTYPE was seen.
         return (fSchemaType != Constants.NS_XMLSCHEMA) && 
-                fValidation && (!fDynamicValidation || fSeenDoctypeDecl)  
-                && (fDTDValidation || fSeenDoctypeDecl);
+               (!fDynamicValidation && fValidation ||
+                fDynamicValidation && fSeenDoctypeDecl) &&
+               (fDTDValidation || fSeenDoctypeDecl);
     }
     
 			//REVISIT:we can convert into functions.. adding default attribute values.. and one validating.
