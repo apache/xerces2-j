d3457eaeb8f313873532c8e6323e9e4b0c3d9ed4
Michael Glavassevich <mrglavas@apache.org>
2003-07-16 16:16:31 +0000

    Recognize ISO-8859-13 (Java supports this encoding), and Latin-9, a registered alias of ISO-8859-15.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@319300 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/util/EncodingMap.java
+++ src/org/apache/xerces/util/EncodingMap.java
@@ -814,8 +814,11 @@ public class EncodingMap {
         fIANA2JavaMap.put("CSISOLATIN5",      "ISO8859_9");
         fIANA2JavaMap.put("L5",      "ISO8859_9");
 
+        fIANA2JavaMap.put("ISO-8859-13",      "ISO8859_13"); 
+        
         fIANA2JavaMap.put("ISO-8859-15",      "ISO8859_15_FDIS"); 
-        fIANA2JavaMap.put("ISO_8859-15",      "ISO8859_15_FDIS"); 
+        fIANA2JavaMap.put("ISO_8859-15",      "ISO8859_15_FDIS");
+        fIANA2JavaMap.put("LATIN-9",          "ISO8859_15_FDIS"); 
 
         fIANA2JavaMap.put("KOI8-R",          "KOI8_R");
         fIANA2JavaMap.put("CSKOI8R",          "KOI8_R");
@@ -904,6 +907,7 @@ public class EncodingMap {
         fJava2IANAMap.put("ISO8859_7",    "ISO-8859-7");
         fJava2IANAMap.put("ISO8859_8",    "ISO-8859-8");
         fJava2IANAMap.put("ISO8859_9",    "ISO-8859-9");
+        fJava2IANAMap.put("ISO8859_13",    "ISO-8859-13");
         fJava2IANAMap.put("ISO8859_15",    "ISO-8859-15");
         fJava2IANAMap.put("ISO8859_15_FDIS",    "ISO-8859-15");
         fJava2IANAMap.put("Big5",      "BIG5");
