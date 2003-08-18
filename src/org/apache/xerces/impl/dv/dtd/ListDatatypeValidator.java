5792c242a756dede91dc2fa84d5c5cf4490ebaaf
Michael Glavassevich <mrglavas@apache.org>
2003-08-18 18:05:19 +0000

    Fixing Bug #20363, incorporates fix for XML SE E20. http://nagoya.apache.org/bugzilla/show_bug.cgi?id=20363.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@319370 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dv/dtd/ListDatatypeValidator.java
+++ src/org/apache/xerces/impl/dv/dtd/ListDatatypeValidator.java
@@ -90,7 +90,7 @@ public class ListDatatypeValidator implements DatatypeValidator {
      */
     public void validate(String content, ValidationContext context) throws InvalidDatatypeValueException {
 
-        StringTokenizer parsedList = new StringTokenizer(content);
+        StringTokenizer parsedList = new StringTokenizer(content," ");
         int numberOfTokens =  parsedList.countTokens();
         if (numberOfTokens == 0) {
             throw new InvalidDatatypeValueException("EmptyList", null);
