47b5876ae7e0c8484495726d4aa3ea28ffd0237d
Elena Litani <elena@apache.org>
2004-01-29 22:54:49 +0000

    Get appropriate DOM implementation for PSVI implementation
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@319731 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/PSVIDocumentImpl.java
+++ src/org/apache/xerces/dom/PSVIDocumentImpl.java
@@ -87,6 +87,18 @@ public class PSVIDocumentImpl extends DocumentImpl {
     public PSVIDocumentImpl(DocumentType doctype) {
         super(doctype);
     }
+    
+    /**
+     * Retrieve information describing the abilities of this particular
+     * DOM implementation. Intended to support applications that may be
+     * using DOMs retrieved from several different sources, potentially
+     * with different underlying representations.
+     */
+    public DOMImplementation getImplementation() {
+        // Currently implemented as a singleton, since it's hardcoded
+        // information anyway.
+        return PSVIDOMImplementationImpl.getDOMImplementation();
+    }
 
     /**
      * Create an element with PSVI information
