81a6f53305a269ff3683a58fe3c602b5b79e2dd0
Michael Glavassevich <mrglavas@apache.org>
2004-04-23 16:06:10 +0000

    Fix indentation in getParameterNames method.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@319897 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/parsers/DOMParserImpl.java
+++ src/org/apache/xerces/parsers/DOMParserImpl.java
@@ -767,43 +767,43 @@ extends AbstractDOMParser implements LSParser, DOMConfiguration {
      * parameter names defined outside this specification.
      */
     public DOMStringList getParameterNames () {
-		if (fRecognizedParameters == null){
-			Vector parameters = new Vector();
-
-			// REVISIT: add Xerces recognized properties/features
-			parameters.add(Constants.DOM_NAMESPACES);
-			parameters.add(Constants.DOM_CDATA_SECTIONS);
-			parameters.add(Constants.DOM_CANONICAL_FORM);
-			parameters.add(Constants.DOM_NAMESPACE_DECLARATIONS);
+        if (fRecognizedParameters == null){
+            Vector parameters = new Vector();
+            
+            // REVISIT: add Xerces recognized properties/features
+            parameters.add(Constants.DOM_NAMESPACES);
+            parameters.add(Constants.DOM_CDATA_SECTIONS);
+            parameters.add(Constants.DOM_CANONICAL_FORM);
+            parameters.add(Constants.DOM_NAMESPACE_DECLARATIONS);
             parameters.add(Constants.DOM_SPLIT_CDATA);
-
-			parameters.add(Constants.DOM_ENTITIES);
-			parameters.add(Constants.DOM_VALIDATE_IF_SCHEMA);
-			parameters.add(Constants.DOM_VALIDATE);
-			parameters.add(Constants.DOM_DATATYPE_NORMALIZATION);
-
-			parameters.add(Constants.DOM_CHARSET_OVERRIDES_XML_ENCODING);
-			parameters.add(Constants.DOM_CHECK_CHAR_NORMALIZATION);
-			parameters.add(Constants.DOM_SUPPORTED_MEDIATYPES_ONLY);
-			parameters.add(Constants.DOM_IGNORE_UNKNOWN_CHARACTER_DENORMALIZATIONS);
-
-			parameters.add(Constants.DOM_NORMALIZE_CHARACTERS);
-			parameters.add(Constants.DOM_WELLFORMED);
-			parameters.add(Constants.DOM_INFOSET);
-			parameters.add(Constants.DOM_DISALLOW_DOCTYPE);
-			parameters.add(Constants.DOM_ELEMENT_CONTENT_WHITESPACE);
-			parameters.add(Constants.DOM_COMMENTS);
-
-			parameters.add(Constants.DOM_ERROR_HANDLER);
-			parameters.add(Constants.DOM_RESOURCE_RESOLVER);
-			parameters.add(Constants.DOM_SCHEMA_LOCATION);
-			parameters.add(Constants.DOM_SCHEMA_TYPE);
-
-			fRecognizedParameters = new DOMStringListImpl(parameters);
-
-		}
-
-		return fRecognizedParameters;
+            
+            parameters.add(Constants.DOM_ENTITIES);
+            parameters.add(Constants.DOM_VALIDATE_IF_SCHEMA);
+            parameters.add(Constants.DOM_VALIDATE);
+            parameters.add(Constants.DOM_DATATYPE_NORMALIZATION);
+            
+            parameters.add(Constants.DOM_CHARSET_OVERRIDES_XML_ENCODING);
+            parameters.add(Constants.DOM_CHECK_CHAR_NORMALIZATION);
+            parameters.add(Constants.DOM_SUPPORTED_MEDIATYPES_ONLY);
+            parameters.add(Constants.DOM_IGNORE_UNKNOWN_CHARACTER_DENORMALIZATIONS);
+            
+            parameters.add(Constants.DOM_NORMALIZE_CHARACTERS);
+            parameters.add(Constants.DOM_WELLFORMED);
+            parameters.add(Constants.DOM_INFOSET);
+            parameters.add(Constants.DOM_DISALLOW_DOCTYPE);
+            parameters.add(Constants.DOM_ELEMENT_CONTENT_WHITESPACE);
+            parameters.add(Constants.DOM_COMMENTS);
+            
+            parameters.add(Constants.DOM_ERROR_HANDLER);
+            parameters.add(Constants.DOM_RESOURCE_RESOLVER);
+            parameters.add(Constants.DOM_SCHEMA_LOCATION);
+            parameters.add(Constants.DOM_SCHEMA_TYPE);
+            
+            fRecognizedParameters = new DOMStringListImpl(parameters);
+            
+        }
+        
+        return fRecognizedParameters;
     }
 
     /**
