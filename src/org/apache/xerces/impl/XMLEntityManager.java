53c7d2f71fe51d9a0358ecebc82ed29d8a2a3c2a
Michael Glavassevich <mrglavas@apache.org>
2004-09-24 18:03:01 +0000

    Adding a convenience method for other classes which wish to resolve URIs against the current user dir.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@320063 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/XMLEntityManager.java
+++ src/org/apache/xerces/impl/XMLEntityManager.java
@@ -1565,6 +1565,18 @@ public class XMLEntityManager
 
         return gUserDirURI;
     }
+    
+    /**
+     * Absolutizes a URI using the current value
+     * of the "user.dir" property as the base URI. If
+     * the URI is already absolute, this is a no-op.
+     * 
+     * @param uri the URI to absolutize
+     */
+    public static void absolutizeAgainstUserDir(URI uri) 
+        throws URI.MalformedURIException {
+        uri.absolutize(getUserDir());
+    }
 
     /**
      * Expands a system id and returns the system id as a URI, if
