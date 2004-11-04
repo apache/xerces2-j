43da2130f1bd23663fd443134eafe2cc81f709cd
Michael Glavassevich <mrglavas@apache.org>
2004-11-04 20:08:55 +0000

    DOM Level 3 specifies an attribute called strictErrorChecking [1] which when set to false allows an implementation to not test every possible error case. Reducing the amount of checking performed when strictErrorChecking is false. This patch is thanks to Naela Nissar.
    [1] http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/core.html#Document3-strictErrorChecking
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@320122 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/AttrImpl.java
+++ src/org/apache/xerces/dom/AttrImpl.java
@@ -337,11 +337,13 @@ public class AttrImpl
      */
     public void setValue(String newvalue) {
 
-    	if (isReadOnly()) {
+    	CoreDocumentImpl ownerDocument = ownerDocument();
+    	
+    	if (ownerDocument.errorChecking && isReadOnly()) {
             String msg = DOMMessageFormatter.formatMessage(DOMMessageFormatter.DOM_DOMAIN, "NO_MODIFICATION_ALLOWED_ERR", null);
             throw new DOMException(DOMException.NO_MODIFICATION_ALLOWED_ERR, msg);
         }
-        CoreDocumentImpl ownerDocument = ownerDocument();
+        
         Element ownerElement = getOwnerElement();
         String oldvalue = "";
         if (needsSyncData()) {
