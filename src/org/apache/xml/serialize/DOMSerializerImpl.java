cbc00a4cb6097b3189e3483711df91380befa140
Michael Glavassevich <mrglavas@apache.org>
2005-03-22 03:40:06 +0000

    Cleanup unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@320254 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/serialize/DOMSerializerImpl.java
+++ src/org/apache/xml/serialize/DOMSerializerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package org.apache.xml.serialize;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
