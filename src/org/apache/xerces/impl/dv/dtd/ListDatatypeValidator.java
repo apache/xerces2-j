ee4ac5fa6eb8d234a728aa2ba5b029161a493aa0
Michael Glavassevich <mrglavas@apache.org>
2005-03-22 05:35:49 +0000

    Moving @xerces.internal before @author.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@320258 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dv/dtd/ListDatatypeValidator.java
+++ src/org/apache/xerces/impl/dv/dtd/ListDatatypeValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2002,2004 The Apache Software Foundation.
+ * Copyright 1999-2002,2004,2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +22,10 @@ import java.util.StringTokenizer;
 /**
  * For list types: ENTITIES, IDREFS, NMTOKENS.
  * 
+ * @xerces.internal
+ * 
  * @author Jeffrey Rodriguez, IBM
  * @author Sandy Gao, IBM
- * 
- * @xerces.internal 
  * 
  * @version $Id$
  */
