1608ddfa9f5f5bb9a3538f29ec807f3d8a5cdfbe
Michael Glavassevich <mrglavas@apache.org>
2005-05-14 20:37:18 +0000

    The RI had one concrete implementation of Schema, creating 4 concrete implementations in Xerces to handle different scenarios.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@320359 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xerces/jaxp/validation/AbstractXMLSchema.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.xerces.jaxp.validation;
+
+import javax.xml.validation.Schema;
+import javax.xml.validation.Validator;
+import javax.xml.validation.ValidatorHandler;
+
+/**
+ * <p>Abstract implementation of Schema for W3C XML Schemas.</p>
+ * 
+ * @author Michael Glavassevich, IBM
+ * @version $Id$
+ */
+abstract class AbstractXMLSchema extends Schema implements
+        XSGrammarPoolContainer {
+
+    /*
+     * Schema methods
+     */
+
+    /* 
+     * @see javax.xml.validation.Schema#newValidator()
+     */
+    public Validator newValidator() {
+        // TODO: Integrate ValidatorImpl from the RI
+        // return new ValidatorImpl(this);
+        return null;
+    }
+
+    /* 
+     * @see javax.xml.validation.Schema#newValidatorHandler()
+     */
+    public ValidatorHandler newValidatorHandler() {
+        // TODO: Integrate ValidatorHandlerImpl from the RI
+        // return new ValidatorHandlerImpl(this);
+        return null;
+    }
+    
+} // AbstractXMLSchema
