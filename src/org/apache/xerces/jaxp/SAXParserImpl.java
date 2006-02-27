b8182c4dfbfb6aa2b6d12209fc87550d119e6912
Michael Glavassevich <mrglavas@apache.org>
2006-02-27 21:06:42 +0000

    Cleanup unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@381441 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/jaxp/SAXParserImpl.java
+++ src/org/apache/xerces/jaxp/SAXParserImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2005 The Apache Software Foundation.
+ * Copyright 2000-2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ import javax.xml.validation.Schema;
 import org.apache.xerces.impl.Constants;
 import org.apache.xerces.impl.validation.ValidationManager;
 import org.apache.xerces.impl.xs.XMLSchemaValidator;
-import org.apache.xerces.impl.xs.XSMessageFormatter;
 import org.apache.xerces.jaxp.validation.XSGrammarPoolContainer;
 import org.apache.xerces.util.SAXMessageFormatter;
 import org.apache.xerces.util.SecurityManager;
