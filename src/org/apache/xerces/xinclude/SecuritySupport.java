5f2ae3790fa0de8c2d821a41363b6d045c939bde
Michael Glavassevich <mrglavas@apache.org>
2006-07-16 21:45:58 +0000

    Eliminate creation of temporary Boolean objects.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@422544 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/xinclude/SecuritySupport.java
+++ src/org/apache/xerces/xinclude/SecuritySupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002,2004 The Apache Software Foundation.
+ * Copyright 2002,2004,2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -130,7 +130,7 @@ final class SecuritySupport {
         return ((Boolean)
                 AccessController.doPrivileged(new PrivilegedAction() {
                     public Object run() {
-                        return new Boolean(f.exists());
+                        return f.exists() ? Boolean.TRUE : Boolean.FALSE;
                     }
                 })).booleanValue();
     }
