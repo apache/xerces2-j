f7f9b4b7ef6e0d7fce8784a8bc5069d58648e572
Michael Glavassevich <mrglavas@apache.org>
2006-09-15 20:43:46 +0000

    Update to the latest ASF license header. See http://www.apache.org/legal/src-headers.html.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@446728 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/xs/opti/DefaultText.java
+++ src/org/apache/xerces/impl/xs/opti/DefaultText.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  * 
  *      http://www.apache.org/licenses/LICENSE-2.0
  * 
@@ -19,10 +20,6 @@ package org.apache.xerces.impl.xs.opti;
 import org.w3c.dom.Text;
 import org.w3c.dom.DOMException;
 
-/*
- * @author Neil Graham, IBM
- * @version $Id$
- */
 /**
  * The <code>Text</code> interface inherits from <code>CharacterData</code> 
  * and represents the textual content (termed character data in XML) of an 
@@ -45,7 +42,11 @@ import org.w3c.dom.DOMException;
  *
  * This is an empty implementation.
  * 
- * @xerces.internal 
+ * @xerces.internal
+ * 
+ * @author Neil Graham, IBM
+ * 
+ * @version $Id$
  */
 public class DefaultText extends NodeImpl implements Text {
 
