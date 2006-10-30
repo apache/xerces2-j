7a964220f0f3c2bcb04ba81fbd6a71464d68d499
Michael Glavassevich <mrglavas@apache.org>
2006-10-30 04:44:22 +0000

    Simplify BooleanDV. The fValueSpace array is unnecessary.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@469063 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dv/xs/BooleanDV.java
+++ src/org/apache/xerces/impl/dv/xs/BooleanDV.java
@@ -30,24 +30,20 @@ import org.apache.xerces.impl.dv.ValidationContext;
  *
  * @version $Id$
  */
-public class BooleanDV extends TypeValidator{
+public class BooleanDV extends TypeValidator {
 
-    private static final String fValueSpace[] = {"false", "true", "0", "1"};
-
-    public short getAllowedFacets(){
+    public short getAllowedFacets() {
         return (XSSimpleTypeDecl.FACET_PATTERN | XSSimpleTypeDecl.FACET_WHITESPACE);
     }
 
     public Object getActualValue(String content, ValidationContext context) throws InvalidDatatypeValueException {
-        Boolean ret = null;
-
-        if (content.equals(fValueSpace[0]) || content.equals(fValueSpace[2]))
-            ret = Boolean.FALSE;
-        else if (content.equals(fValueSpace[1]) || content.equals(fValueSpace[3]))
-            ret = Boolean.TRUE;
-        else
-            throw new InvalidDatatypeValueException("cvc-datatype-valid.1.2.1", new Object[]{content, "boolean"});
-        return ret;
+        if ("false".equals(content) || "0".equals(content)) {
+            return Boolean.FALSE;
+        }
+        else if ("true".equals(content) || "1".equals(content)) {
+            return Boolean.TRUE;
+        }
+        throw new InvalidDatatypeValueException("cvc-datatype-valid.1.2.1", new Object[]{content, "boolean"});
     }
 
 } // class BooleanDV
