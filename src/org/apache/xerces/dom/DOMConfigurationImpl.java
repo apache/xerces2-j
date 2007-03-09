d757c10b66391b3371a29d9e4c42ae975cd88710
Michael Glavassevich <mrglavas@apache.org>
2007-03-09 04:55:33 +0000

    When support for DTD validation support was added to DOMNormalizer we forgot to add "http://www.w3.org/TR/REC-xml" as an allowable value for the "schema-type" property. The entity resolver property wasn't included in the parameter list returned from getParameterNames() and was not accounted for in canSetParameter(). Both of these minor bugs should be fixed now.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@516299 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/DOMConfigurationImpl.java
+++ src/org/apache/xerces/dom/DOMConfigurationImpl.java
@@ -1036,8 +1036,12 @@ public class DOMConfigurationImpl extends ParserConfigurationSettings
         }
         else if (name.equalsIgnoreCase(Constants.DOM_SCHEMA_TYPE)) {
             // REVISIT: should null value be supported?
-            //as of now we are only supporting W3C XML Schema
-            return ( (value instanceof String) && value.equals(Constants.NS_XMLSCHEMA) ) ? true : false ;
+            // as of now we are only supporting W3C XML Schema and DTD.
+            return ((value instanceof String) && 
+                    (value.equals(Constants.NS_XMLSCHEMA) || value.equals(Constants.NS_DTD))) ? true : false;
+        }
+        else if (name.equalsIgnoreCase(ENTITY_RESOLVER)) {
+            return (value instanceof XMLEntityResolver) ? true : false;
         }
         else if (name.equalsIgnoreCase(SYMBOL_TABLE)){
             // Xerces Symbol Table
@@ -1061,47 +1065,48 @@ public class DOMConfigurationImpl extends ParserConfigurationSettings
      * can be set by the application. Note that this list can also contain
      * parameter names defined outside this specification.
      */
-    public DOMStringList getParameterNames() {
-    	if (fRecognizedParameters == null){
-			Vector parameters = new Vector();
+	public DOMStringList getParameterNames() {
+	    if (fRecognizedParameters == null){
+	        Vector parameters = new Vector();
 
-			//Add DOM recognized parameters
-			//REVISIT: Would have been nice to have a list of
-			//recognized paramters.
-			parameters.add(Constants.DOM_COMMENTS);
-			parameters.add(Constants.DOM_DATATYPE_NORMALIZATION);
-			parameters.add(Constants.DOM_CDATA_SECTIONS);
-			parameters.add(Constants.DOM_ENTITIES);
-			parameters.add(Constants.DOM_SPLIT_CDATA);
-			parameters.add(Constants.DOM_NAMESPACES);
-			parameters.add(Constants.DOM_VALIDATE);
+	        //Add DOM recognized parameters
+	        //REVISIT: Would have been nice to have a list of
+	        //recognized paramters.
+	        parameters.add(Constants.DOM_COMMENTS);
+	        parameters.add(Constants.DOM_DATATYPE_NORMALIZATION);
+	        parameters.add(Constants.DOM_CDATA_SECTIONS);
+	        parameters.add(Constants.DOM_ENTITIES);
+	        parameters.add(Constants.DOM_SPLIT_CDATA);
+	        parameters.add(Constants.DOM_NAMESPACES);
+	        parameters.add(Constants.DOM_VALIDATE);
 
-			parameters.add(Constants.DOM_INFOSET);
-			parameters.add(Constants.DOM_NORMALIZE_CHARACTERS);
-			parameters.add(Constants.DOM_CANONICAL_FORM);
-			parameters.add(Constants.DOM_VALIDATE_IF_SCHEMA);
-			parameters.add(Constants.DOM_CHECK_CHAR_NORMALIZATION);
-			parameters.add(Constants.DOM_WELLFORMED);
+	        parameters.add(Constants.DOM_INFOSET);
+	        parameters.add(Constants.DOM_NORMALIZE_CHARACTERS);
+	        parameters.add(Constants.DOM_CANONICAL_FORM);
+	        parameters.add(Constants.DOM_VALIDATE_IF_SCHEMA);
+	        parameters.add(Constants.DOM_CHECK_CHAR_NORMALIZATION);
+	        parameters.add(Constants.DOM_WELLFORMED);
 
-			parameters.add(Constants.DOM_NAMESPACE_DECLARATIONS);
-			parameters.add(Constants.DOM_ELEMENT_CONTENT_WHITESPACE);
+	        parameters.add(Constants.DOM_NAMESPACE_DECLARATIONS);
+	        parameters.add(Constants.DOM_ELEMENT_CONTENT_WHITESPACE);
 
-			parameters.add(Constants.DOM_ERROR_HANDLER);
-			parameters.add(Constants.DOM_SCHEMA_TYPE);
-			parameters.add(Constants.DOM_SCHEMA_LOCATION);
-			parameters.add(Constants.DOM_RESOURCE_RESOLVER);
+	        parameters.add(Constants.DOM_ERROR_HANDLER);
+	        parameters.add(Constants.DOM_SCHEMA_TYPE);
+	        parameters.add(Constants.DOM_SCHEMA_LOCATION);
+	        parameters.add(Constants.DOM_RESOURCE_RESOLVER);
 
-			//Add recognized xerces features and properties
-			parameters.add(GRAMMAR_POOL);
-			parameters.add(SYMBOL_TABLE);
-			parameters.add(SEND_PSVI);
+	        //Add recognized xerces features and properties
+	        parameters.add(ENTITY_RESOLVER);
+	        parameters.add(GRAMMAR_POOL);
+	        parameters.add(SYMBOL_TABLE);
+	        parameters.add(SEND_PSVI);
 
-			fRecognizedParameters = new DOMStringListImpl(parameters);
+	        fRecognizedParameters = new DOMStringListImpl(parameters);
 
-    	}
+	    }
 
-    	return fRecognizedParameters;
-    }//getParameterNames
+	    return fRecognizedParameters;
+	}//getParameterNames
 
     //
     // Protected methods
