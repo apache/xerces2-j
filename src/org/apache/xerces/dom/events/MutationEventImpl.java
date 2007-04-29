2832ff5c77732e86bd3c8d3074b166e48dad1fe6
Michael Glavassevich <mrglavas@apache.org>
2007-04-29 17:24:58 +0000

    Formatting clean-up.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@533531 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/dom/events/MutationEventImpl.java
+++ src/org/apache/xerces/dom/events/MutationEventImpl.java
@@ -21,21 +21,25 @@ import org.w3c.dom.Node;
 import org.w3c.dom.events.MutationEvent;
 
 /**
+ * An implementation of the DOM Level 2 <code>MutationEvent</code> interface.
+ * 
  * @xerces.internal 
  * 
  * @version $Id$
  */
-
 public class MutationEventImpl 
-extends org.apache.xerces.dom.events.EventImpl 
-implements MutationEvent
-{
-    Node relatedNode=null;
-    String prevValue=null,newValue=null,attrName=null;
+    extends EventImpl 
+    implements MutationEvent {
+    
+    Node relatedNode = null;
+    String prevValue = null;
+    String newValue = null;
+    String attrName = null;
+
     // REVISIT: The DOM Level 2 PR has a bug: the init method should let this
     // attribute be specified. Since it doesn't we have to give write access.
     public short attrChange;
-    
+
     // NON-DOM CONSTANTS: Storage efficiency, avoid risk of typos.
     public static final String DOM_SUBTREE_MODIFIED = "DOMSubtreeModified";
     public static final String DOM_NODE_INSERTED = "DOMNodeInserted";
@@ -45,66 +49,64 @@ implements MutationEvent
     public static final String DOM_ATTR_MODIFIED = "DOMAttrModified";
     public static final String DOM_CHARACTER_DATA_MODIFIED = "DOMCharacterDataModified";
 
-    /** @return the name of the Attr which
-        changed, for DOMAttrModified events. 
-        Undefined for others.
-        */
-    public String getAttrName()
-    {
+    /** 
+     * @return the name of the Attr which
+     * changed, for DOMAttrModified events. 
+     * Undefined for others.
+     */
+    public String getAttrName() {
         return attrName;
     }
 
     /**
-     *  <code>attrChange</code> indicates the type of change which triggered 
+     * <code>attrChange</code> indicates the type of change which triggered 
      * the DOMAttrModified event. The values can be <code>MODIFICATION</code>
      * , <code>ADDITION</code>, or <code>REMOVAL</code>. 
      */
-    public short getAttrChange()
-    {
+    public short getAttrChange() {
         return attrChange;
     }
 
-    /** @return the new string value of the Attr for DOMAttrModified events, or
-        of the CharacterData node for DOMCharDataModifed events.
-        Undefined for others.
-        */
-    public String getNewValue()
-    {
+    /** 
+     * @return the new string value of the Attr for DOMAttrModified events, or
+     * of the CharacterData node for DOMCharDataModifed events.
+     * Undefined for others.
+     */
+    public String getNewValue() {
         return newValue;
     }
 
-    /** @return the previous string value of the Attr for DOMAttrModified events, or
-        of the CharacterData node for DOMCharDataModifed events.
-        Undefined for others.
-        */
-    public String getPrevValue()
-    {
+    /** 
+     * @return the previous string value of the Attr for DOMAttrModified events, or
+     * of the CharacterData node for DOMCharDataModifed events.
+     * Undefined for others.
+     */
+    public String getPrevValue() {
         return prevValue;
     }
 
-    /** @return a Node related to this event, other than the target that the
-        node was dispatched to. For DOMNodeRemoved, it is the node which
-        was removed. 
-        No other uses are currently defined.
-        */
-    public Node getRelatedNode()
-    {
+    /** 
+     * @return a Node related to this event, other than the target that the
+     * node was dispatched to. For DOMNodeRemoved, it is the node which
+     * was removed. 
+     * No other uses are currently defined.
+     */
+    public Node getRelatedNode() {
         return relatedNode;
     }
 
-    /** Initialize a mutation event, or overwrite the event's current
-        settings with new values of the parameters. 
-        */
+    /** 
+     * Initialize a mutation event, or overwrite the event's current
+     * settings with new values of the parameters. 
+     */
     public void initMutationEvent(String typeArg, boolean canBubbleArg, 
-        boolean cancelableArg, Node relatedNodeArg, String prevValueArg, 
-        String newValueArg, String attrNameArg, short attrChangeArg)
-    {
-        relatedNode=relatedNodeArg;
-        prevValue=prevValueArg;
-        newValue=newValueArg;
-        attrName=attrNameArg;
-        attrChange=attrChangeArg;
-        super.initEvent(typeArg,canBubbleArg,cancelableArg);
+            boolean cancelableArg, Node relatedNodeArg, String prevValueArg, 
+            String newValueArg, String attrNameArg, short attrChangeArg) {
+        relatedNode = relatedNodeArg;
+        prevValue = prevValueArg;
+        newValue = newValueArg;
+        attrName = attrNameArg;
+        attrChange = attrChangeArg;
+        super.initEvent(typeArg, canBubbleArg, cancelableArg);
     }
-
 }
