1b35c16e54334bbfda96c6e6eeb55e44297ee1cd
Michael Glavassevich <mrglavas@apache.org>
2009-06-15 19:10:21 +0000

    Applying SVN rev 784913 to the XML Schema 1.1 branch: Fix for time comparison (only hours, minutes and seconds) should be used.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@784922 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dv/xs/TimeDV.java
+++ src/org/apache/xerces/impl/dv/xs/TimeDV.java
@@ -78,6 +78,7 @@ public class TimeDV extends AbstractDateTimeDV {
         
         if ( date.utc!=0 && date.utc != 'Z') {
             normalize(date);
+            date.day = 15;
         }
         date.position = 2;
         return date;
