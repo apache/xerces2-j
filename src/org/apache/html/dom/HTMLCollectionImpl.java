b9338cfc7157af6acd20edf47cdeb4c68bbbbb6b
Michael Glavassevich <mrglavas@apache.org>
2009-12-14 05:20:31 +0000

    JIRA Issue #1386: http://issues.apache.org/jira/browse/XERCESJ-1386. By request, making HTMLCollectionImpl implement java.io.Serializable so that the HTML DOM can be serialized to an ObjectOutputStream when the nodes contain instances of this class.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@890183 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/html/dom/HTMLCollectionImpl.java
+++ src/org/apache/html/dom/HTMLCollectionImpl.java
@@ -17,6 +17,8 @@
 
 package org.apache.html.dom;
 
+import java.io.Serializable;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.html.HTMLAnchorElement;
@@ -59,9 +61,10 @@ import org.w3c.dom.html.HTMLTableSectionElement;
  * @see org.w3c.dom.html.HTMLCollection
  */
 class HTMLCollectionImpl
-    implements HTMLCollection
+    implements HTMLCollection, Serializable
 {
-    
+
+    private static final long serialVersionUID = 9112122196669185082L;
 
     /**
      * Request collection of all anchors in document: &lt;A&gt; elements that
