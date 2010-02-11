2190ea375bf9e95fc0317287f49a21965e74af5c
Khaled Noaman <knoaman@apache.org>
2010-02-11 20:31:12 +0000

    Fix problem in Regex, in normal mode with multi-line option, when last character is a new line and we are matching a '^'. The match should fail.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/trunk@909133 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/xpath/regex/RegularExpression.java
+++ src/org/apache/xerces/impl/xpath/regex/RegularExpression.java
@@ -959,7 +959,7 @@ public class RegularExpression implements java.io.Serializable {
                 case '^':
                     if (isSet(opts, MULTIPLE_LINES)) {
                         if (!(offset == con.start
-                              || offset > con.start && isEOLChar( target [  offset-1 ] )))
+                              || offset > con.start && offset < con.limit && isEOLChar( target [  offset-1 ] )))
                             return -1;
                     } else {
                         if (offset != con.start)
@@ -1532,13 +1532,7 @@ public class RegularExpression implements java.io.Serializable {
      */
     private int matchString (Context con, Op op, int offset, int dx, int opts) {
 
-
-
-
         String target = con.strTarget;
-
-
-
 
         while (true) {
             if (op == null)
@@ -1646,7 +1640,7 @@ public class RegularExpression implements java.io.Serializable {
                 case '^':
                     if (isSet(opts, MULTIPLE_LINES)) {
                         if (!(offset == con.start
-                              || offset > con.start && isEOLChar( target .charAt(  offset-1 ) )))
+                              || offset > con.start && offset < con.limit && isEOLChar( target .charAt(  offset-1 ) )))
                             return -1;
                     } else {
                         if (offset != con.start)
@@ -2258,7 +2252,7 @@ public class RegularExpression implements java.io.Serializable {
                 case '^':
                     if (isSet(opts, MULTIPLE_LINES)) {
                         if (!(offset == con.start
-                              || offset > con.start && isEOLChar( target .setIndex(  offset-1 ) )))
+                              || offset > con.start && offset < con.limit && isEOLChar( target .setIndex(  offset-1 ) )))
                             return -1;
                     } else {
                         if (offset != con.start)
