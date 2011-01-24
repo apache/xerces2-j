13e7f564494e3fbd67fdad4515e8cab2381004db
Michael Glavassevich <mrglavas@apache.org>
2011-01-24 04:45:26 +0000

    Making ElementState static. It does not need access to the enclosing class.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@1062647 13f79535-47bb-0310-9956-ffa450edef68

--- samples/xni/PSVIWriter.java
+++ samples/xni/PSVIWriter.java
@@ -2385,7 +2385,7 @@ public class PSVIWriter implements XMLComponent, XMLDocumentFilter {
         return (schemaValue != null) ? schemaValue.getNormalizedValue() : null;
     } // getSchemaNormalizedValue(ItemPSVI)
 
-    class ElementState {
+    static final class ElementState {
         public boolean isEmpty;
         XMLAttributes fAttributes;
 
