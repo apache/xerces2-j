1e129123991754b4e77ef25a504799af38d8aee1
Mukul Gandhi <mukulg@apache.org>
2012-01-19 01:26:26 +0000

    schema 1.1 commit: prohibiting the attributes "minOccurs" and "maxOccurs" to appear on an <any> wild-card if it exists within <openContent> or <defaultOpenContent>. this is as per the "schema for schema" for XSD 1.1.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@1233147 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/xs/traversers/XSDComplexTypeTraverser.java
+++ src/org/apache/xerces/impl/xs/traversers/XSDComplexTypeTraverser.java
@@ -318,6 +318,13 @@ class  XSDComplexTypeTraverser extends XSDAbstractParticleTraverser {
                 fAttrChecker.returnAttrArray(attrValues, schemaDoc);
                 return ocDecl;
             }
+            Attr anyWcMinOccurs = child.getAttributeNode(SchemaSymbols.ATT_MINOCCURS);
+            Attr anyWcMaxOccurs = child.getAttributeNode(SchemaSymbols.ATT_MAXOCCURS);
+            if (anyWcMinOccurs != null || anyWcMaxOccurs != null) {
+                // prohibit the attributes "minOccurs" and "maxOccurs" to appear on an <any> wild-card if it exists within <openContent> or <defaultOpenContent>
+                // REVISIT...
+                reportSchemaError("s4s-att-not-allowed", new Object[]{elmNode.getLocalName()+"=>"+SchemaSymbols.ELT_ANY, SchemaSymbols.ATT_MINOCCURS+"|"+SchemaSymbols.ATT_MAXOCCURS}, child); 
+            }
             Object[] wcAttrValues = fAttrChecker.checkAttributes(child, false, schemaDoc);
             ocDecl.fWildcard = fSchemaHandler.fWildCardTraverser.traverseWildcardDecl(child, wcAttrValues, schemaDoc, grammar);
             fAttrChecker.returnAttrArray(wcAttrValues, schemaDoc);
