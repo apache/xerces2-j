4dcbb9618139d764a05e546753fdc3dcfce482d8
Shudi Gao <sandygao@apache.org>
2012-08-09 15:23:53 +0000

    Fixing potential NPE. Target namespace of a schema component could be null.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@1371254 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/xs/traversers/XSDHandler.java
+++ src/org/apache/xerces/impl/xs/traversers/XSDHandler.java
@@ -3404,7 +3404,7 @@ public class XSDHandler {
     
     private void addRelatedType(XSTypeDefinition type, Vector componentList, String namespace, Hashtable dependencies) {
         if (!type.getAnonymous()) {
-            if (!type.getNamespace().equals(SchemaSymbols.URI_SCHEMAFORSCHEMA)) { //REVISIT - do we use == instead
+            if (!SchemaSymbols.URI_SCHEMAFORSCHEMA.equals(type.getNamespace())) { //REVISIT - do we use == instead
                 if (!componentList.contains(type)) {
                     final Vector importedNamespaces = findDependentNamespaces(namespace, dependencies);
                     addNamespaceDependency(namespace, type.getNamespace(), importedNamespaces);
