a6af9ac7325d61af608de1fa0c40ca0ac0ab0609
Michael Glavassevich <mrglavas@apache.org>
2012-08-20 15:26:19 +0000

    Reducing visibility of some constants.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@1375057 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/dtd/XMLDTDLoader.java
+++ src/org/apache/xerces/impl/dtd/XMLDTDLoader.java
@@ -97,11 +97,11 @@ public class XMLDTDLoader
         Constants.XERCES_PROPERTY_PREFIX + Constants.ERROR_HANDLER_PROPERTY;
 
     /** Property identifier: entity resolver. */
-    public static final String ENTITY_RESOLVER =
+    protected static final String ENTITY_RESOLVER =
         Constants.XERCES_PROPERTY_PREFIX + Constants.ENTITY_RESOLVER_PROPERTY;
     
     /** Property identifier: locale. */
-    public static final String LOCALE =
+    protected static final String LOCALE =
         Constants.XERCES_PROPERTY_PREFIX + Constants.LOCALE_PROPERTY;
 
     /** Recognized properties. */
