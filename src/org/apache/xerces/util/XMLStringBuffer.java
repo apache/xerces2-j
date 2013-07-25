f0b34db8fa45daab1ae164dc9cd4c171e5639403
Michael Glavassevich <mrglavas@apache.org>
2013-07-25 18:14:29 +0000

    Improving code formatting.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@1507082 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/util/XMLStringBuffer.java
+++ src/org/apache/xerces/util/XMLStringBuffer.java
@@ -111,12 +111,13 @@ public class XMLStringBuffer
      */
     public void append(char c) {
         if (this.length + 1 > this.ch.length) {
-                    int newLength = this.ch.length*2;
-                    if (newLength < this.ch.length + DEFAULT_SIZE)
-                        newLength = this.ch.length + DEFAULT_SIZE;
-                    char[] newch = new char[newLength];
-                    System.arraycopy(this.ch, 0, newch, 0, this.length);
-                    this.ch = newch;
+            int newLength = this.ch.length * 2;
+            if (newLength < this.ch.length + DEFAULT_SIZE) {
+                newLength = this.ch.length + DEFAULT_SIZE;
+            }
+            char[] newch = new char[newLength];
+            System.arraycopy(this.ch, 0, newch, 0, this.length);
+            this.ch = newch;
         }
         this.ch[this.length] = c;
         this.length++;
